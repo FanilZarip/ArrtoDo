94787f9d01c00e661cc7258f53929366baae537d
FanilZarip <fanilfan1994@mail.ru>
2021-11-16 02:14:33 +0300

    ArrToDO

--- toDoWithArray.js
+++ toDoWithArray.js
@@ -6,7 +6,7 @@ const list = [];
 let idN = 1;
 
 function addTask(name, status, priority, id = idN) {
-	list.push({ idN, name, status, priority });
+	list.push({ id, name, status, priority });
 	idN++;
 }
 
@@ -37,25 +37,35 @@ function showList() {
 	let PROGRESS = 'InProgress';
 	let DONE = 'Done';
 
-	function showStatusList(status, title) {
+	function showListByStatus(status, title) {
+		let fullTaskInfo;
 		let count = 0;
-		function getTODOList(item, index, array) {
+		function getList(item, index, array) {
 			if (item.status == status) {
 				count = count + 1;
 				return console.log(item.name);
+
+				// return item.name; // Так можно увидеть задачу как объект
 			}
 		}
 		console.log('\n', title);
-		list.filter(getTODOList);
+
+		// Так можно увидеть задачу как объект
+		// fullTaskInfo = list.filter(getList);
+		// console.log(fullTaskInfo);
+		//
+
+		list.filter(getList);
+
 		// console.log(count);
 		if (count === 0) {
 			console.log(emptyTaskList);
 		}
 	}
 
-	showStatusList(TODO, toDOTitle);
-	showStatusList(PROGRESS, ProgressTitle);
-	showStatusList(DONE, DoneTitle);
+	showListByStatus(TODO, toDOTitle);
+	showListByStatus(PROGRESS, ProgressTitle);
+	showListByStatus(DONE, DoneTitle);
 }
 
 addTask('test', 'TODO', 'high');
